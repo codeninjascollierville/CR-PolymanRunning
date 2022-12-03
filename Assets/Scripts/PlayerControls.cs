88777ab0e23ed4a20fccc0007d5a7711b5ad0ba9
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2022-12-03 11:32:05 -0600

    E

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -13,12 +13,41 @@ public class PlayerControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+        }
+    }
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObj;
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 1f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("InstantiateObjects", spawnTime, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = new Vector3(15, -2.75f, 0);
     }
+    void InstantiateObjects()
+        {
+        Instantiate(challengeObj, transform.position, transform.rotation);
+        }
 }
